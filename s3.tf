1510ceadf315feaa8b0815e07975b0f19dd2ef36
mayank0202 <mayank.sharma@sourcefuse.com>
2022-10-13 17:23:47 +0530

    fix

--- s3.tf
+++ s3.tf
@@ -20,7 +20,10 @@ resource "aws_s3_bucket" "this" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_acl" "this" {
