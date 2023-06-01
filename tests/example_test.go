4f61bb25f94aaadaad2822f94af9447cbce9a282
mayank0202 <mayank.sharma@sourcefuse.com>
2023-06-01 22:41:54 +0530

    updated test

--- tests/example_test.go
+++ tests/example_test.go
@@ -20,7 +20,6 @@ func TestTerraformExample(t *testing.T) {
 	// Assert
 	assert := assert.New(t)
 
-	outputValue := terraform.Output(t, terraformOptions, "output_name")
+	outputValue := terraform.Output(t, terraformOptions, "cloudfront_domain_name")
 	assert.NotNil(outputValue)
-	assert.Equal("output_value", outputValue)
 }
