0042080d9cec73768104dfe5ef295e22ee701ccb
Vijay John stephen <vijay.stephen@sourcefuse.com>
2023-07-26 18:56:05 +0530

    #major * Fix for ACM region

--- version.tf
+++ version.tf
@@ -3,8 +3,9 @@ terraform {
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+      source                = "hashicorp/aws"
+      version               = "~> 4.0"
+      configuration_aliases = [aws.acm]
     }
   }
 }
