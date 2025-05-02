70179dbce0acb8d5ede8475450d823931fab5e24
Manikanta <manikanta.sadurla@sourcefuse.com>
2025-05-02 14:13:09 +0530

    updated output

--- examples/s3-origin/output.tf
+++ examples/s3-origin/output.tf
@@ -1,4 +1,12 @@
-output "cloudfront_domain_name" {
-  value       = module.cloudfront.cloudfront_domain_name
-  description = "CloudFront Domain name"
+# output "cloudfront_domain_name" {
+#   value       = module.cloudfront.cloudfront_domain_name
+#   description = "CloudFront Domain name"
+# }
+
+output "cloudfront_domain_names" {
+  value = {
+    for key, mod in module.cloudfront :
+    key => mod.cloudfront_domain_name
+  }
+  description = "CloudFront Domain names from all distributions"
 }
